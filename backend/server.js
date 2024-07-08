c20c41d1c7c3c851f26197506f5d37baa5af703e
Deepak Mahapatra <deepakmahapatra76@gmail.com>
2024-07-08 21:52:44 +0530

    connect to database

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,26 @@ const port = process.env.PORT || 4444;
 
 const uri = process.env.ATLAS_URI;
 
+async function run() {
+    const client = new MongoClient(uri);
+    await client.connect();
+    const dbName = 'Inventory';
+    const collectionName = 'Food';
+    const database = client.db(dbName);
+    const collection = database.collection(collectionName);
+
+    try {
+        const trial = await collection.insertMany([{name: 'apple', quantity: 3},{name: 'banana', quantity: 1}]);
+        console.log(`${trial.insertedCount} documents updated \n`);
+    } catch (err) {
+        console.error(err);
+    }
+
+    await client.close();
+}
+
+run().catch(console.dir);
+
 app.use(cors());
 
 app.listen(port, () => console.log(`server is live on port: ${port}`));  
